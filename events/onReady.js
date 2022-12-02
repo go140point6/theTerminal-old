9ba9c52bab696bdd85988bdaf9623494c69aaae2
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-12-02 14:06:54 -0800

    d

--- events/onReady.js
+++ events/onReady.js
@@ -11,6 +11,9 @@ const Database = require('better-sqlite3');
 function onReady(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`)
     
+    let guild = client.get_guild(id);
+    console.log(guild);
+
     client.commands = new Collection();
 
     const commands = [];
