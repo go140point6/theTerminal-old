e68b9b8853d3e5f30c6b83fe6bfd817914f137ad
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-03 15:14:00 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -54,12 +54,12 @@ module.exports = {
                     let rawAmount = (offer.buy[0].Amount)
                     //Number(rawAmount);
                     //console.log(typeof Number(amount));
-                    console.log(rawAmount / 1000000);
+                    console.log(rawAmount / 100000);
 
 
                     
                     //console.log(offer);
-                    embedFields.push({ name: offer.buy[0].NFTokenID, value: amount})
+                    embedFields.push({ name: offer.buy[0].NFTokenID, value: "two"})
                 }
                })
 
