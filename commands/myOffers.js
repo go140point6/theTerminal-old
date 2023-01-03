b0b5d89220f28473567c8706590c08b0da6ced60
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-03 14:39:10 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -40,7 +40,20 @@ module.exports = {
                });
 
                console.log(results);
-            }
-        })
+
+               const embedOffers = new EmbedBuilder()
+               .setColor('DarkRed')
+               .setTitle(`Welcome to The Terminal`)
+               //.setAuthor({ name: client.user.username })
+               .setDescription(`The query results for ${address}:`)
+               .setThumbnail(client.user.avatarURL())
+               .addFields(embedFields)
+               //.setImage('https://onxrp-marketplace.s3.us-east-2.amazonaws.com/nft-images/00081AF4B6C6354AE81B765895498071D5E681DB44D3DE8F1589271700000598-32c83d6e902f8.png')
+               .setTimestamp()
+               //.setFooter({ text: 'Powered by OnTheDex.Live', iconURL: 'https://images2.imgbox.com/bb/cc/OJPcux6J_o.jpg' });
+
+               interaction.editReply({ embeds: [embedOffers]});
+            } 
+         })
     }
 };
