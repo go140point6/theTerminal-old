cabe0de340a9d8c6172852c40f8701f89e1a116e
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-09 15:59:00 -0800

    d

--- events/onInteraction.js
+++ events/onInteraction.js
@@ -1,11 +1,20 @@
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, SelectMenuBuilder, ButtonStyle } = require('discord.js');
+const axios = require('axios');
+const client = require('../index');
+
 async function onInteraction(interaction) {
     if (interaction.isChatInputCommand()) {
-        console.log(interaction);
+        //console.log(interaction);
         const command = interaction.client.commands.get(interaction.commandName);
         await command.execute(interaction);
     } else if (interaction.isButton()) {
-        console.log(interaction);
-        interaction.reply({ content: 'Don\'t touch me there!' });
+
+        //console.log(interaction);
+        //interaction.reply({ content: 'Don\'t touch me there!' });
+        interaction.reply({ content: `${address}` })
+
+
+
     } else {
         return;
     }
