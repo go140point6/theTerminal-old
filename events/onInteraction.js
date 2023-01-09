0d8f585415c20f6c7b8ecb726209baae060db2a5
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-09 14:25:55 -0800

    f

--- events/onInteraction.js
+++ events/onInteraction.js
@@ -4,7 +4,7 @@ async function onInteraction(interaction) {
         const command = interaction.client.commands.get(interaction.commandName);
         await command.execute(interaction);
     } else if (interaction.isButton()) {
-        //console.log(interaction);
+        console.log(interaction);
         interaction.reply({ content: 'Don\'t touch me there!' });
     } else {
         return;
