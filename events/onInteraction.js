2c7d91a56df6c2fe992dd8bed5909564fe99833a
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-09 13:17:52 -0800

    d

--- events/onInteraction.js
+++ events/onInteraction.js
@@ -1,8 +1,10 @@
 async function onInteraction(interaction) {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-    await command.execute(interaction);
+    if (interaction.isChatInputCommand()) {
+        const command = interaction.client.commands.get(interaction.commandName);
+        await command.execute(interaction);
+    } else {
+        return;
+    }
 };
 
 module.exports = { 
