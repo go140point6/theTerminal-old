8a0a812ba9ff07effdedb37612c85c72e4dd8ab0
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-10 11:43:49 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -69,7 +69,7 @@ module.exports = {
             //.setFooter({ text: 'Powered by OnTheDex.Live', iconURL: 'https://images2.imgbox.com/bb/cc/OJPcux6J_o.jpg' });
             
 	    //await i.update({ content: 'A button was clicked!', components: [] });
-        await i.update({ embeds: [editEmbed], components: [] });
+        await i.editReply({ embeds: [editEmbed], components: [] });
     });
 
     collector.on('end', collected => console.log(`Collected ${collected.size} items`));
