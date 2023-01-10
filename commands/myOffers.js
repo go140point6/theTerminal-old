78417195cba99e79b45024e560bfe217132ae628
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-10 11:28:52 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -38,7 +38,16 @@ module.exports = {
             //.setFooter({ text: 'Powered by OnTheDex.Live', iconURL: 'https://images2.imgbox.com/bb/cc/OJPcux6J_o.jpg' });
 
     await interaction.reply({ embeds: [embed], components: [row] });
-    //const MSG = await message.channel.send({embeds: [embed], components: [row]});
-    
+
+    const filter = i => i.customId === 'buy';
+
+    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+    collector.on('collect', async i => {
+	    await i.update({ content: 'A button was clicked!', components: [] });
+    });
+
+    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+
     },
 };
