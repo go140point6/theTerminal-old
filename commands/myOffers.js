dbd5cc7c7ec1a32edbd60e0dce0e6b7721ab5401
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-11 15:52:47 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -118,13 +118,12 @@ module.exports = {
               
 
             const editBuyEmbed = new EmbedBuilder()
-                console.log(embedFields)
                 .setColor('DarkRed')
                 .setTitle(`Welcome to The Terminal`)
                 //.setAuthor({ name: client.user.username })
                 .setDescription(`Current BUY offers for ${address}`)
                 .setThumbnail(client.user.avatarURL())
-                //.addFields(embedFields)
+                .addFields(embedFields)
                 //.setImage('https://onxrp-marketplace.s3.us-east-2.amazonaws.com/nft-images/00081AF4B6C6354AE81B765895498071D5E681DB44D3DE8F1589271700000598-32c83d6e902f8.png')
                 .setTimestamp()
                 //.setFooter({ text: 'Powered by OnTheDex.Live', iconURL: 'https://images2.imgbox.com/bb/cc/OJPcux6J_o.jpg' });
