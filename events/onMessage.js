d083b08f5bc40cd61ed8682eb9aa4ceeafa54eb5
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-11 11:07:15 -0800

    d

--- events/onMessage.js
+++ events/onMessage.js
@@ -1,7 +1,8 @@
 async function onMessage(message) {
     if(message.author.bot) return;
     
-    message.reply("Bingo")
+    //message.reply("Bingo")
+    console.log(message);
 };
 
 module.exports = { 
