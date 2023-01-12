8545e225c3524035a85713d65f59c61de40fc692
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-12 11:09:12 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -115,7 +115,8 @@ module.exports = {
                 for (var key in offers) {
                     if (offers[key].buy.length !== 0) {
                         for (var key1 in offers[key]) {
-                            console.log(offers[key][key1])
+                            console.log(offers[key][key1].NFTokenID)
+                            console.log(offers[key][key1].Amount)
                         }
                     }      
                 }
