523a61a600b1ce21d022594dabefc128009f6dc2
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-12 11:55:44 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -116,7 +116,9 @@ module.exports = {
                     //console.log(offer.buy[0])
                     Object.entries(offer.buy).forEach(([key, val]) => {
                         //console.log(`${key} ${val}`)
-                        console.log(val)
+                        console.log(val.NFTokenID)
+                        console.log(val.Amount)
+                        console.log(val.Flags)
                     })
                 })
 
