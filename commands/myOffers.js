8a7bf676c8188f076112619063667f3371633cb2
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-12 12:59:52 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -129,6 +129,7 @@ module.exports = {
                         //)
                         //console.log(findNFTIndex)
                         currentOffers.push({ "id": offer.buy[key].NFTokenID, "amount": offer.buy[key].Amount })
+                        console.log(typeof(offer.buy[key].NFTTokenID))
                         console.log(currentOffers.length)
                         const findNFTIndex = currentOffers.findIndex(
                             (nftid) => nftid.id === offer.buy[key].NFTTokenID
