312d7d1d65156e793edf7e86dd139ed25faffb08
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-12 11:41:13 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -114,7 +114,7 @@ module.exports = {
                 
                 offers.forEach(offer => {
                     for (var key in offer) {
-                        console.log(offer.buy[key])
+                        console.log(offer[key])
                     }
                     //console.log(offer.buy[0])
                 })
