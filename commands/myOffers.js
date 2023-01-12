9e012a20e7837c98bf0391008f90cd67247dba34
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-11 16:19:00 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -94,7 +94,7 @@ module.exports = {
         }
     });
 
-    async function buyOffers() {
+    async function buyOffers(i) {
         await axios.get(`https://api.xrpldata.com/api/v1/xls20-nfts/offers/nftowner/${address}`).then(res => {
             if(res.data) {
                 let offers = res.data.data.offers;
