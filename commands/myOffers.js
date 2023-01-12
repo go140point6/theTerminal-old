c55a0493528076e49ad445aee69fcb8cafe4b627
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-11 16:15:35 -0800

    f

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -2,6 +2,39 @@ const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, Butt
 const axios = require('axios');
 const client = require('../index');
 
+async function buyOffers() {
+    await axios.get(`https://api.xrpldata.com/api/v1/xls20-nfts/offers/nftowner/${address}`).then(res => {
+        if(res.data) {
+            let offers = res.data.data.offers;
+            let embedFields = [];
+
+            offers.forEach(offer => {
+             if (Object.keys(offer.buy).length !== 0) {
+                 let rawAmount = (offer.buy[0].Amount)
+                 let amount = (Number(rawAmount))/1000000;
+
+                 embedFields.push({ name: offer.buy[0].NFTokenID, value: amount.toString()})
+                }
+            })
+
+        const editBuyEmbed = new EmbedBuilder()
+            .setColor('DarkRed')
+            .setTitle(`Welcome to The Terminal`)
+            //.setAuthor({ name: client.user.username })
+            .setDescription(`Current BUY offers for ${address}`)
+            .setThumbnail(client.user.avatarURL())
+            .addFields(embedFields)
+            //.setImage('https://onxrp-marketplace.s3.us-east-2.amazonaws.com/nft-images/00081AF4B6C6354AE81B765895498071D5E681DB44D3DE8F1589271700000598-32c83d6e902f8.png')
+            .setTimestamp()
+            //.setFooter({ text: 'Powered by OnTheDex.Live', iconURL: 'https://images2.imgbox.com/bb/cc/OJPcux6J_o.jpg' });
+    
+        //await i.update({ content: 'A button was clicked!', components: [] });
+        i.update({ embeds: [editBuyEmbed], components: [] });
+        collector.stop('Collector stopped manually');
+        }
+    })
+};
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('myoffers')
@@ -41,15 +74,13 @@ module.exports = {
     await interaction.reply({ embeds: [initialEmbed], components: [row] });
     console.log(interaction.user.id);
     
-    //const filter = i => i.user.id === interaction.user.id;
-    
     const collector = interaction.channel.createMessageComponentCollector({ componentType: ComponentType.Button, time: 5000 });
 
     collector.on('collect', async i => {
         if (i.user.id === interaction.user.id && i.customId === 'buy') {
             
             console.log('Address to check: ' + address);
-            //buyOffers();
+            buyOffers();
 
          } else if (i.user.id === interaction.user.id && i.customId === 'sell') {
           
@@ -96,37 +127,4 @@ module.exports = {
         }
     });
     }
-};
-
-async function buyOffers() {
-    await axios.get(`https://api.xrpldata.com/api/v1/xls20-nfts/offers/nftowner/${address}`).then(res => {
-        if(res.data) {
-            let offers = res.data.data.offers;
-            let embedFields = [];
-
-            offers.forEach(offer => {
-             if (Object.keys(offer.buy).length !== 0) {
-                 let rawAmount = (offer.buy[0].Amount)
-                 let amount = (Number(rawAmount))/1000000;
-
-                 embedFields.push({ name: offer.buy[0].NFTokenID, value: amount.toString()})
-                }
-            })
-
-        const editBuyEmbed = new EmbedBuilder()
-            .setColor('DarkRed')
-            .setTitle(`Welcome to The Terminal`)
-            //.setAuthor({ name: client.user.username })
-            .setDescription(`Current BUY offers for ${address}`)
-            .setThumbnail(client.user.avatarURL())
-            .addFields(embedFields)
-            //.setImage('https://onxrp-marketplace.s3.us-east-2.amazonaws.com/nft-images/00081AF4B6C6354AE81B765895498071D5E681DB44D3DE8F1589271700000598-32c83d6e902f8.png')
-            .setTimestamp()
-            //.setFooter({ text: 'Powered by OnTheDex.Live', iconURL: 'https://images2.imgbox.com/bb/cc/OJPcux6J_o.jpg' });
-    
-        //await i.update({ content: 'A button was clicked!', components: [] });
-        i.update({ embeds: [editBuyEmbed], components: [] });
-        collector.stop('Collector stopped manually');
-        }
-    })
 };
