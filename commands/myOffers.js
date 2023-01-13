1c9903ceb4f7be6a88384fb92bae2e78bd47a46d
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-13 09:08:49 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -48,8 +48,8 @@ module.exports = {
         if (i.user.id === interaction.user.id && i.customId === 'buy') {
             
             console.log('Address to check: ' + address);
-            const getIPFS = hexToString(hexToString);
-            console.log(getIPFS)
+            const getIPFS = hexToString();
+            //console.log(getIPFS)
             buyOffers(i);
 
          } else if (i.user.id === interaction.user.id && i.customId === 'sell') {
--- utils/hexToString.js
+++ utils/hexToString.js
@@ -3,6 +3,7 @@ var hexToString = function () {
     const hexToUtf8 = convert('hex', 'utf8')
 
     hexToUtf8('516D6232585954637A66444A356E59753555746679517679794C3770524463727A4D513667536F35356233487841')
+    console.log(hexToString)
 };
 
 exports.hexToString = hexToString;
