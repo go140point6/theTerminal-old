8a3a7733859b039869aec95d55b4295034c6b281
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-19 09:27:23 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -185,10 +185,10 @@ module.exports = {
                                 "Expiration": offer.buy[key].Expiration
                             }
                         }
-                        currentOffers.push({ "id": offer.buy[key].NFTokenID, "offerNo": key, "amount": offer.buy[key].Amount })
+                        //currentOffers.push({ "id": offer.buy[key].NFTokenID, "offerNo": key, "amount": offer.buy[key].Amount })
                         //currentOffers.push({ "id": offer.buy[key].NFTokenID, "amount": offer.buy[key].Amount })
-                        console.log(currentOffers.length)
-                        console.log(currentOffers)
+                        //console.log(currentOffers.length)
+                        //console.log(currentOffers)
                     })
                 })
 
