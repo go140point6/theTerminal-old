42c1af34fa54fa169d76b40494b54a429c38ce1c
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-19 11:51:28 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -283,7 +283,7 @@ module.exports = {
                         { name: '2', value: '2', inline: true },
                     )
                     */
-                    //.addFields({ name: `Showing offer ${currentOffers[1].NFTokenID}`, value: amount.toString(), inline: false })
+                    .addFields({ name: `Showing offer ${currentOffers[1].NFTokenID}`, value: amount.toString(), inline: false })
                     //.addFields({ name: `1`, value: '1', inline: false })
                     //.addFields({ name: `2`, value: '2', inline: false })
                     //.setImage(`https://marketplace-api.onxrp.com/api/image/${currentOffers[1].NFTokenID}?thumbnail=true`)
