094d8504f3974664202136028148ef44cbd06617
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-18 16:29:11 -0800

    d

--- utils/xrpl-example.js
+++ utils/xrpl-example.js
@@ -7,7 +7,7 @@ async function main() {
     "command": "account_nfts",
     "account": "rM7kvwoahaMN4zrF5fd5TuPoxkJJZCB3a8",
   })
-  console.log(response.id)
+  console.log(response.result.account_nfts)
 
   client.disconnect()
 }
