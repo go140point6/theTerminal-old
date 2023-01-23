d16fad4f4ef56134e5b6e0cac2a86a30b0fcaea8
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-01-23 14:52:33 -0800

    d

--- commands/myOffers.js
+++ commands/myOffers.js
@@ -61,7 +61,7 @@ module.exports = {
             .setTitle(`Welcome to The Terminal`)
             //.setAuthor({ name: client.user.username })
             //.setDescription(`I will show you the current HIGHEST BUY offers for this address.`)
-            .setDescription(`${i.user.username} has requestd to see the current HIGHEST BID offers for address ${address}.  Processing...`)
+            .setDescription(`${interaction.user.username} has requestd to see the current HIGHEST BID offers for address ${address}.  Processing...`)
             .setThumbnail(client.user.avatarURL())
             //.addFields(embedFields)
             //.setImage('https://onxrp-marketplace.s3.us-east-2.amazonaws.com/nft-images/00081AF4B6C6354AE81B765895498071D5E681DB44D3DE8F1589271700000598-32c83d6e902f8.png')
